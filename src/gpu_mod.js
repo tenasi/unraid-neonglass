4d056dcbd1f41fdb10aedda878dba9c8114706fa
jonpetersathan <jonpetersathan@gmail.com>
2024-12-25 23:01:17 +0000

    Fix selection bug in gpu_mod.js

--- src/gpu_mod.js
+++ src/gpu_mod.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     let gpu_bus_row=Array.from(document.querySelectorAll('tr')).filter(tr => tr.textContent.includes('PCI Bus Rx/Tx (MB/s)')).children[0];
 
     if (gpu_bus_row.length > 0) {
-        gpu_bus_row[0].hidden=true;
+        gpu_bus_row.hidden=true;
     }
 });
